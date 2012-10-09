6d2701afd50a8770cf3720bc69f66ca8e023e0a9
AnhDT <dotheanh@kgoin.com>
2012-10-09 10:36:09 +0700

    begin project hilita vietnam

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+		<title>Home - Vòng bi</title>
+		<link type="text/css" href="css/style.css" rel="stylesheet" />
+		<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+		<script src="js/bjqs-1.3.min.js"></script>
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="header">
+				<div class="logo"></div>
+				<div class="navMenu">
+					
+				</div>
+			</div><!--.HEADER-->
+			<div id="container">
+			</div><!--.CONTAINER-->
+			<div id="footer">
+			</div><!--.FOOTER-->
+		</div><!--.WRAPPER-->
+	</body>
+</html>
